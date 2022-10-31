032ba9faafb78b75b83352bcf1158076dc6fe237
Brian Zoetewey <brian.zoetewey@cru.org>
2022-10-31 11:54:10 -0400

    Update application for ECS multi-container

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-docker buildx build $DOCKER_ARGS \
-    --build-arg SECRET_KEY_BASE=$SECRET_KEY_BASE \
-    --build-arg DD_API_KEY=$DD_API_KEY \
-    .
+docker buildx build $DOCKER_ARGS .
